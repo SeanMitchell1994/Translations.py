a781363708e5bdc2a66a63c94be02b32f197916d
Sean Mitchell <sm0077@uah.edu>
2019-09-11 15:33:05 -0500

    Updated driver.py.

--- src/driver.py
+++ src/driver.py
@@ -1,4 +1,11 @@
 from trans import Trans
 
 rotate_obj = Trans()
+print("2d rotation:")
 print(rotate_obj.rotate_2d(30))
+print("\n3d rotation (x):")
+print(rotate_obj.rotate_x_3d(45))
+print("\n3d rotation (y):")
+print(rotate_obj.rotate_y_3d(45))
+print("\n3d rotation (z):")
+print(rotate_obj.rotate_z_3d(45))
